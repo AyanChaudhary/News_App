4c0ee4dce1454cbf776e843b8b3f93bdce42faca
Mohammed Ayan <112795104+AyanChaudhary@users.noreply.github.com>
2023-07-15 15:47:42 +0530

    Update NewsApi.kt

--- app/src/main/java/com/example/newsly/Api/NewsApi.kt
+++ app/src/main/java/com/example/newsly/Api/NewsApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 //https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7b192c8f07604b2eb215585a3c47cfb6
-const val API_KEY="7b192c8f07604b2eb215585a3c47cfb6"
+const val API_KEY=""
 interface NewsApi {
     @GET("v2/top-headlines?apiKey=$API_KEY")
     suspend fun getBreakingNews(@Query("country")country:String="in",@Query("page")pageNumber:Int=1):Response<News>
